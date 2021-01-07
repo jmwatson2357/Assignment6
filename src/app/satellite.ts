304ba74afa23527b9e17a11f569d982f1b3317a1
jmwatson2357 <54993615+jmwatson2357@users.noreply.github.com>
2021-01-06 19:56:05 -0600

    changed AppComponent to fetch satellite data

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,15 +1,16 @@
 export class Satellite {
     name: string;
-    orbitType: string;
     type: string;
     operational: boolean;
+    orbitType: string;
     launchDate: string;
 
-    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
+    constructor(name: string, type: string, operational: boolean, orbitType: string, launchDate: string){
         this.name = name;
         this.type = type;
-        this.launchDate = launchDate;
-        this.orbitType = orbitType;
         this.operational = operational;
+        this.orbitType = orbitType;
+        this.launchDate = launchDate;
+        
     }
 }
